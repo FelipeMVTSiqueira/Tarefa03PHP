adb2c363487b5e500bbea3e95cd5d3f562065232
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-09 16:21:04 -0300

    resolucao ex 3 tarefa 3

--- tarefa03.php
+++ tarefa03.php
@@ -1,5 +1,11 @@
 <?php
-    for($i=0;$i<=100;$i++){
+    for($i=0;$i<=mt_rand(1,100);$i++){
         echo $i."<br>";
+    }
+?>
+
+<?php 
+    for($io=0;$io<=10;$io++){
+        echo "2 x ".$io." = ".(2*$io)."<br>";
     }
 ?>
