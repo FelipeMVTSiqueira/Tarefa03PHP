3abdba573ef66aea01c72ed85ba8368ecf2e02ad
Felipe Siqueira <felipesiqueira1212@gmail.com>
2019-10-09 23:37:24 -0300

    resolucao exercicio da moeda com while ex4T3

--- tarefa03.php
+++ tarefa03.php
@@ -1,11 +1,32 @@
 <?php
-    for($i=0;$i<=mt_rand(1,100);$i++){
+    for($i=0;$i<=rand(1,100);$i++){
         echo $i."<br>";
-    }
+    } echo "<br>"."<br>";
 ?>
 
 <?php 
     for($io=0;$io<=10;$io++){
         echo "2 x ".$io." = ".(2*$io)."<br>";
-    }
+    } echo "<br>"."<br>";
+?>
+
+
+
+
+<?php
+    $qntCara = 0;
+    $qntLancamento = 0;
+    while ($qntCara < 5) {
+        $moeda = rand(0,1);
+        $qntLancamento ++;
+        if ($moeda == 1){
+            $qntCara++;
+            echo "Cara"."<br>"."<br>";
+        }
+        else {
+            $qntCara;
+            echo "Coroa"."<br>"."<br>";
+            }
+        }
+echo "Levaram ".$qntLancamento." lançamentos!"."<br>"."<br>";
 ?>
